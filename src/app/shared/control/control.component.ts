d78b1d8bce769fe68b5082d24f95f7250b8eaab8
Lorenzo Donato <lorenzodonato75@gmail.com>
2026-02-13 13:50:17 -0300

    feat: dynamic css

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, HostBinding, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -6,8 +6,20 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+    '(click)': 'onClick()',
+    
+  }
 })
 export class ControlComponent {
-  label = input.required<string>()
+  // @HostBinding('class') className = 'control';
+  label = input.required<string>();
+  private el = inject(ElementRef)
+
+  onClick() {
+    console.log('Clicked!');
+    console.log(this.el);
+  }
 }
