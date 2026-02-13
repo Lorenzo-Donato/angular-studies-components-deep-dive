00b6f2df4b07f7498938543d77272b80d2199751
Lorenzo <lorenzodonato75@gmail.com>
2026-02-13 10:34:37 -0300

    feat: organized big html index into smaller components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { Header } from './header/header';
 import { ServerStatusComponent } from './server-status/server-status.component';
 import { TrafficComponent } from './traffic/traffic.component';                                                             
 import { TicketsComponent } from './tickets/tickets.component';
+import { DashboardItemComponent } from './dashboard/dashboard-item/dashboard-item.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Header, ServerStatusComponent, TrafficComponent, TicketsComponent],
+  imports: [Header, ServerStatusComponent, TrafficComponent, TicketsComponent, DashboardItemComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
